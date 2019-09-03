4d9105a598234724ea43cfda48d70aad46b38b6a
Jesse Luehrs <doy@tozt.net>
2019-09-03 05:43:40 -0400

    refactor process to handle async a bit better

--- src/process.rs
+++ src/process.rs
@@ -1,7 +1,6 @@
 use futures::future::Future as _;
 use snafu::ResultExt as _;
-use std::io::{Read as _, Write as _};
-use tokio::io::AsyncRead as _;
+use tokio::io::{AsyncRead as _, AsyncWrite as _};
 use tokio_pty_process::CommandExt as _;
 
 #[derive(Debug, snafu::Snafu)]
@@ -14,6 +13,9 @@ pub enum Error {
 
     #[snafu(display("failed to resize pty: {}", source))]
     ResizePty { source: std::io::Error },
+
+    #[snafu(display("failed to read from pty: {}", source))]
+    ReadFromPty { source: std::io::Error },
 
     #[snafu(display("failed to write to pty: {}", source))]
     WriteToPty { source: std::io::Error },
@@ -51,11 +53,11 @@ pub struct Process {
     // TODO: tokio::io::Stdin is broken
     // input: tokio::io::Stdin,
     input: tokio::reactor::PollEvented2<EventedStdin>,
+    input_buf: std::collections::VecDeque<u8>,
     cmd: String,
     args: Vec<String>,
     buf: Vec<u8>,
     started: bool,
-    output_done: bool,
     exit_done: bool,
     manage_screen: bool,
     raw_screen: Option<crossterm::RawScreen>,
@@ -105,11 +107,11 @@ impl Process {
             pty,
             process,
             input,
+            input_buf: std::collections::VecDeque::with_capacity(4096),
             cmd: cmd.to_string(),
             args: args.to_vec(),
-            buf: Vec::with_capacity(4096),
+            buf: vec![0; 4096],
             started: false,
-            output_done: false,
             exit_done: false,
             manage_screen: true,
             raw_screen: None,
@@ -125,10 +127,6 @@ impl Process {
     fn poll_command_start(
         &mut self,
     ) -> futures::Poll<Option<CommandEvent>, Error> {
-        if self.started {
-            return Ok(futures::Async::NotReady);
-        }
-
         self.started = true;
         Ok(futures::Async::Ready(Some(CommandEvent::CommandStart(
             self.cmd.clone(),
@@ -138,42 +136,69 @@ impl Process {
 
     fn poll_read_stdin(
         &mut self,
-    ) -> futures::Poll<Option<CommandEvent>, Error> {
+    ) -> Option<futures::Poll<Option<CommandEvent>, Error>> {
+        // XXX this is why i had to do the EventedFd thing - poll_read on its
+        // own will block reading from stdin, so i need a way to explicitly
+        // check readiness before doing the read
         let ready = mio::Ready::readable();
-        let input_poll = self.input.poll_read_ready(ready);
-        if let Ok(futures::Async::Ready(_)) = input_poll {
-            let stdin = std::io::stdin();
-            let mut stdin = stdin.lock();
-            // TODO: async
-            let n = stdin.read(&mut self.buf).context(ReadFromTerminal)?;
-            if n > 0 {
-                let bytes = self.buf[..n].to_vec();
+        match self.input.poll_read_ready(ready) {
+            Ok(futures::Async::Ready(_)) => {
+                match self.input.poll_read(&mut self.buf) {
+                    Ok(futures::Async::Ready(n)) => {
+                        if n > 0 {
+                            self.input_buf.extend(self.buf[..n].iter());
+                        }
+                    }
+                    Ok(futures::Async::NotReady) => {
+                        return Some(Ok(futures::Async::NotReady));
+                    }
+                    Err(e) => return Some(Err(e).context(ReadFromTerminal)),
+                }
+                // XXX i'm pretty sure this is wrong (if the single poll_read
+                // call didn't return all waiting data, clearing read ready
+                // state means that we won't get the rest until some more data
+                // beyond that appears), but i don't know that there's a way
+                // to do it correctly given that poll_read blocks
+                if let Err(e) = self
+                    .input
+                    .clear_read_ready(ready)
+                    .context(PtyClearReadReady)
+                {
+                    return Some(Err(e));
+                }
 
-                // TODO: async
-                self.pty.write_all(&bytes).context(WriteToPty)?;
+                None
             }
+            Ok(futures::Async::NotReady) => {
+                Some(Ok(futures::Async::NotReady))
+            }
+            Err(e) => Some(Err(e).context(ReadFromTerminal)),
         }
-        // TODO: this could lose pending bytes if there is stuff to read in
-        // the buffer but we don't read it all in the previous read call,
-        // since i think we won't get another notification until new bytes
-        // actually arrive even if there are bytes in the buffer
-        self.input
-            .clear_read_ready(ready)
-            .context(PtyClearReadReady)?;
+    }
 
-        Ok(futures::Async::NotReady)
+    fn poll_write_stdin(
+        &mut self,
+    ) -> Option<futures::Poll<Option<CommandEvent>, Error>> {
+        let (a, b) = self.input_buf.as_slices();
+        let buf = if a.is_empty() { b } else { a };
+        match self.pty.poll_write(buf) {
+            Ok(futures::Async::Ready(n)) => {
+                for _ in 0..n {
+                    self.input_buf.pop_front();
+                }
+                None
+            }
+            Ok(futures::Async::NotReady) => {
+                Some(Ok(futures::Async::NotReady))
+            }
+            Err(e) => Some(Err(e).context(WriteToPty)),
+        }
     }
 
     fn poll_read_stdout(
         &mut self,
     ) -> futures::Poll<Option<CommandEvent>, Error> {
-        if self.output_done {
-            return Ok(futures::Async::NotReady);
-        }
-
-        self.buf.clear();
-        let output_poll = self.pty.read_buf(&mut self.buf);
-        match output_poll {
+        match self.pty.poll_read(&mut self.buf) {
             Ok(futures::Async::Ready(n)) => {
                 let bytes = self.buf[..n].to_vec();
                 let bytes: Vec<_> = bytes
@@ -191,25 +216,14 @@ impl Process {
                 Ok(futures::Async::Ready(Some(CommandEvent::Output(bytes))))
             }
             Ok(futures::Async::NotReady) => Ok(futures::Async::NotReady),
-            Err(_) => {
-                // explicitly ignoring errors (for now?) because we
-                // always read off the end of the pty after the process
-                // is done
-                self.output_done = true;
-                Ok(futures::Async::NotReady)
-            }
+            Err(e) => Err(e).context(ReadFromPty),
         }
     }
 
     fn poll_command_exit(
         &mut self,
     ) -> futures::Poll<Option<CommandEvent>, Error> {
-        if self.exit_done {
-            return Ok(futures::Async::NotReady);
-        }
-
-        let exit_poll = self.process.poll().context(ProcessExitPoll);
-        match exit_poll {
+        match self.process.poll().context(ProcessExitPoll) {
             Ok(futures::Async::Ready(status)) => {
                 self.exit_done = true;
                 Ok(futures::Async::Ready(Some(CommandEvent::CommandExit(
@@ -234,39 +248,67 @@ impl futures::stream::Stream for Process {
             );
         }
 
-        match self.poll_command_start() {
-            r @ Ok(futures::Async::Ready(_)) => return r,
-            e @ Err(_) => return e,
-            _ => {}
-        }
+        loop {
+            let mut should_continue = false;
 
-        match self.poll_read_stdin() {
-            r @ Ok(futures::Async::Ready(_)) => return r,
-            e @ Err(_) => return e,
-            _ => {}
-        }
+            if !self.started {
+                match self.poll_command_start() {
+                    r @ Ok(futures::Async::Ready(_)) => return r,
+                    e @ Err(_) => return e,
+                    Ok(futures::Async::NotReady) => {}
+                }
+            }
 
-        match self.poll_read_stdout() {
-            r @ Ok(futures::Async::Ready(_)) => return r,
-            e @ Err(_) => return e,
-            _ => {}
-        }
+            if !self.exit_done {
+                match self.poll_command_exit() {
+                    r @ Ok(futures::Async::Ready(_)) => return r,
+                    e @ Err(_) => return e,
+                    Ok(futures::Async::NotReady) => {}
+                }
 
-        match self.poll_command_exit() {
-            r @ Ok(futures::Async::Ready(_)) => return r,
-            e @ Err(_) => return e,
-            _ => {}
-        }
+                match self.poll_read_stdin() {
+                    Some(r @ Ok(futures::Async::Ready(_))) => return r,
+                    Some(e @ Err(_)) => return e,
+                    None => should_continue = true,
+                    Some(Ok(futures::Async::NotReady)) => {}
+                }
 
-        if self.exit_done {
-            Ok(futures::Async::Ready(None))
-        } else {
-            Ok(futures::Async::NotReady)
+                if !self.input_buf.is_empty() {
+                    match self.poll_write_stdin() {
+                        Some(r @ Ok(futures::Async::Ready(_))) => return r,
+                        Some(e @ Err(_)) => return e,
+                        None => should_continue = true,
+                        Some(Ok(futures::Async::NotReady)) => {}
+                    }
+                }
+
+                match self.poll_read_stdout() {
+                    r @ Ok(futures::Async::Ready(_)) => return r,
+                    e @ Err(_) => return e,
+                    Ok(futures::Async::NotReady) => {}
+                }
+            }
+
+            if should_continue {
+                continue;
+            } else if self.exit_done {
+                return Ok(futures::Async::Ready(None));
+            } else {
+                return Ok(futures::Async::NotReady);
+            }
         }
     }
 }
 
 struct EventedStdin;
+
+impl std::io::Read for EventedStdin {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let stdin = std::io::stdin();
+        let mut stdin = stdin.lock();
+        stdin.read(buf)
+    }
+}
 
 impl mio::Evented for EventedStdin {
     fn register(

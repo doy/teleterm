e2104548034a9941823569ddd18982cf1e696784
Jesse Luehrs <doy@tozt.net>
2019-09-29 09:34:10 -0400

    add boilerplate for the watch implementation

--- src/cmd/watch.rs
+++ src/cmd/watch.rs
@@ -1,3 +1,4 @@
+use futures::future::Future as _;
 use snafu::ResultExt as _;
 
 #[derive(Debug, snafu::Snafu)]
@@ -19,13 +20,22 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 pub fn cmd<'a, 'b>(app: clap::App<'a, 'b>) -> clap::App<'a, 'b> {
     app.about("Watch termcast streams")
+        .arg(clap::Arg::with_name("id"))
 }
 
-pub fn run<'a>(_matches: &clap::ArgMatches<'a>) -> super::Result<()> {
-    run_impl().context(super::Watch)
+pub fn run<'a>(matches: &clap::ArgMatches<'a>) -> super::Result<()> {
+    run_impl(matches.value_of("id")).context(super::Watch)
 }
 
-fn run_impl() -> Result<()> {
+fn run_impl(id: Option<&str>) -> Result<()> {
+    if let Some(id) = id {
+        watch(id)
+    } else {
+        list()
+    }
+}
+
+fn list() -> Result<()> {
     let sock =
         std::net::TcpStream::connect("127.0.0.1:8000").context(Connect)?;
     let term = std::env::var("TERM").unwrap_or_else(|_| "".to_string());
@@ -40,7 +50,7 @@ fn run_impl() -> Result<()> {
         crate::protocol::Message::Sessions { ids } => {
             println!("available sessions:");
             for id in ids {
-                println!("got id '{}'", id);
+                println!("{}", id);
             }
         }
         _ => {
@@ -49,4 +59,67 @@ fn run_impl() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn watch(id: &str) -> Result<()> {
+    tokio::run(
+        WatchSession::new(
+            id,
+            "127.0.0.1:8000",
+            "doy",
+            std::time::Duration::from_secs(5),
+        )?
+        .map_err(|e| {
+            eprintln!("{}", e);
+        }),
+    );
+
+    Ok(())
+}
+
+struct WatchSession {
+    id: String,
+    client: crate::client::Client,
+}
+
+impl WatchSession {
+    const POLL_FNS: &'static [&'static dyn for<'a> Fn(
+        &'a mut Self,
+    ) -> Result<
+        crate::component_future::Poll<()>,
+    >] = &[&Self::poll_do_something];
+
+    fn new(
+        id: &str,
+        address: &str,
+        username: &str,
+        heartbeat_duration: std::time::Duration,
+    ) -> Result<Self> {
+        let client = crate::client::Client::new(
+            address,
+            username,
+            crate::client::Type::Watching,
+            heartbeat_duration,
+        );
+        Ok(Self {
+            id: id.to_string(),
+            client,
+        })
+    }
+
+    fn poll_do_something(
+        &mut self,
+    ) -> Result<crate::component_future::Poll<()>> {
+        Ok(crate::component_future::Poll::NothingToDo)
+    }
+}
+
+#[must_use = "futures do nothing unless polled"]
+impl futures::future::Future for WatchSession {
+    type Item = ();
+    type Error = Error;
+
+    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error> {
+        crate::component_future::poll_future(self, Self::POLL_FNS)
+    }
 }

262cb794dc805aa820b08d22b43df5258cc6ef4b
Jesse Luehrs <doy@tozt.net>
2019-09-30 11:25:17 -0400

    make the server listen on 0.0.0.0 by default

--- src/cmd/server.rs
+++ src/cmd/server.rs
@@ -22,7 +22,7 @@ pub fn cmd<'a, 'b>(app: clap::App<'a, 'b>) -> clap::App<'a, 'b> {
 }
 
 pub fn run<'a>(matches: &clap::ArgMatches<'a>) -> super::Result<()> {
-    run_impl(matches.value_of("address").unwrap_or("127.0.0.1:4144"))
+    run_impl(matches.value_of("address").unwrap_or("0.0.0.0:4144"))
         .context(super::Server)
 }
 

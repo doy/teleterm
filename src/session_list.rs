60e887dc52b2274405d058d63d5d98f7fc10129b
Jesse Luehrs <doy@tozt.net>
2019-10-09 04:23:06 -0400

    add some tests for the session list

--- src/session_list.rs
+++ src/session_list.rs
@@ -295,3 +295,239 @@ fn truncate(s: &str, len: usize) -> String {
         format!("{}...", &s[..(len - 3)])
     }
 }
+
+#[cfg(test)]
+#[allow(clippy::cognitive_complexity)]
+#[allow(clippy::redundant_clone)]
+#[allow(clippy::shadow_unrelated)]
+mod test {
+    use super::*;
+
+    fn session(username: &str, idle: u32) -> crate::protocol::Session {
+        crate::protocol::Session {
+            id: format!("{}", uuid::Uuid::new_v4()),
+            username: username.to_string(),
+            term_type: "screen".to_string(),
+            size: crate::term::Size { rows: 24, cols: 80 },
+            idle_time: idle,
+            title: "title".to_string(),
+        }
+    }
+
+    #[test]
+    fn test_session_list_sorting() {
+        let size = crate::term::Size { rows: 24, cols: 80 };
+
+        let session1 = session("doy", 35);
+        let session2 = session("doy", 3);
+        let mut session3 = session("sartak", 12);
+        let session4 = session("sartak", 100);
+        let mut session5 = session("toft", 5);
+        let mut sessions = vec![
+            session1.clone(),
+            session2.clone(),
+            session3.clone(),
+            session4.clone(),
+            session5.clone(),
+        ];
+
+        assert_eq!(
+            SessionList::new(sessions.clone(), size.clone()).sessions,
+            vec![
+                session2.clone(),
+                session1.clone(),
+                session5.clone(),
+                session3.clone(),
+                session4.clone(),
+            ]
+        );
+
+        session3.idle_time = 2;
+        sessions[2].idle_time = 2;
+        assert_eq!(
+            SessionList::new(sessions.clone(), size.clone()).sessions,
+            vec![
+                session3.clone(),
+                session4.clone(),
+                session2.clone(),
+                session1.clone(),
+                session5.clone(),
+            ]
+        );
+
+        session5.idle_time = 1;
+        sessions[4].idle_time = 1;
+        assert_eq!(
+            SessionList::new(sessions.clone(), size.clone()).sessions,
+            vec![
+                session5.clone(),
+                session3.clone(),
+                session4.clone(),
+                session2.clone(),
+                session1.clone(),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_session_list_pagination() {
+        let size = crate::term::Size { rows: 11, cols: 80 };
+        let sessions = vec![
+            session("doy", 0),
+            session("doy", 1),
+            session("doy", 2),
+            session("doy", 3),
+            session("doy", 4),
+            session("doy", 5),
+            session("doy", 6),
+            session("doy", 7),
+            session("doy", 8),
+            session("doy", 9),
+            session("doy", 10),
+        ];
+        let mut list = SessionList::new(sessions.clone(), size);
+        assert_eq!(list.limit(), 5);
+        assert_eq!(list.total_pages(), 3);
+        assert_eq!(list.current_page(), 1);
+
+        list.next_page();
+        assert_eq!(list.limit(), 5);
+        assert_eq!(list.total_pages(), 3);
+        assert_eq!(list.current_page(), 2);
+
+        list.next_page();
+        assert_eq!(list.limit(), 5);
+        assert_eq!(list.total_pages(), 3);
+        assert_eq!(list.current_page(), 3);
+
+        list.next_page();
+        assert_eq!(list.limit(), 5);
+        assert_eq!(list.total_pages(), 3);
+        assert_eq!(list.current_page(), 3);
+
+        list.prev_page();
+        assert_eq!(list.limit(), 5);
+        assert_eq!(list.total_pages(), 3);
+        assert_eq!(list.current_page(), 2);
+
+        list.prev_page();
+        assert_eq!(list.limit(), 5);
+        assert_eq!(list.total_pages(), 3);
+        assert_eq!(list.current_page(), 1);
+
+        list.prev_page();
+        assert_eq!(list.limit(), 5);
+        assert_eq!(list.total_pages(), 3);
+        assert_eq!(list.current_page(), 1);
+
+        let id = list.id_for('a').unwrap();
+        assert_eq!(id, sessions[0].id);
+        let id = list.id_for('e').unwrap();
+        assert_eq!(id, sessions[4].id);
+        let id = list.id_for('f');
+        assert!(id.is_none());
+
+        list.next_page();
+        let id = list.id_for('a').unwrap();
+        assert_eq!(id, sessions[5].id);
+
+        list.next_page();
+        let id = list.id_for('a').unwrap();
+        assert_eq!(id, sessions[10].id);
+        let id = list.id_for('b');
+        assert!(id.is_none());
+
+        let size = crate::term::Size { rows: 24, cols: 80 };
+        let sessions = vec![
+            session("doy", 0),
+            session("doy", 1),
+            session("doy", 2),
+            session("doy", 3),
+            session("doy", 4),
+            session("doy", 5),
+            session("doy", 6),
+            session("doy", 7),
+            session("doy", 8),
+            session("doy", 9),
+            session("doy", 10),
+            session("doy", 11),
+            session("doy", 12),
+            session("doy", 13),
+            session("doy", 14),
+            session("doy", 15),
+            session("doy", 16),
+            session("doy", 17),
+            session("doy", 18),
+            session("doy", 19),
+            session("doy", 20),
+            session("doy", 21),
+        ];
+        let list = SessionList::new(sessions.clone(), size);
+        assert_eq!(list.limit(), 18);
+        assert_eq!(list.total_pages(), 2);
+        assert_eq!(list.current_page(), 1);
+
+        let id = list.id_for('a').unwrap();
+        assert_eq!(id, sessions[0].id);
+        let id = list.id_for('p').unwrap();
+        assert_eq!(id, sessions[15].id);
+        let id = list.id_for('q');
+        assert!(id.is_none());
+        let id = list.id_for('r').unwrap();
+        assert_eq!(id, sessions[16].id);
+        let id = list.id_for('s').unwrap();
+        assert_eq!(id, sessions[17].id);
+        let id = list.id_for('t');
+        assert!(id.is_none());
+    }
+
+    #[test]
+    fn test_truncate() {
+        assert_eq!(truncate("abcdefghij", 12), "abcdefghij");
+        assert_eq!(truncate("abcdefghij", 11), "abcdefghij");
+        assert_eq!(truncate("abcdefghij", 10), "abcdefghij");
+        assert_eq!(truncate("abcdefghij", 9), "abcdef...");
+        assert_eq!(truncate("abcdefghij", 8), "abcde...");
+        assert_eq!(truncate("abcdefghij", 7), "abcd...");
+
+        assert_eq!(truncate("", 7), "");
+        assert_eq!(truncate("a", 7), "a");
+        assert_eq!(truncate("ab", 7), "ab");
+        assert_eq!(truncate("abc", 7), "abc");
+        assert_eq!(truncate("abcd", 7), "abcd");
+        assert_eq!(truncate("abcde", 7), "abcde");
+        assert_eq!(truncate("abcdef", 7), "abcdef");
+        assert_eq!(truncate("abcdefg", 7), "abcdefg");
+        assert_eq!(truncate("abcdefgh", 7), "abcd...");
+        assert_eq!(truncate("abcdefghi", 7), "abcd...");
+        assert_eq!(truncate("abcdefghij", 7), "abcd...");
+    }
+
+    #[test]
+    fn test_format_time() {
+        assert_eq!(format_time(0), "0s");
+        assert_eq!(format_time(5), "5s");
+        assert_eq!(format_time(10), "10s");
+        assert_eq!(format_time(60), "1m00s");
+        assert_eq!(format_time(61), "1m01s");
+        assert_eq!(format_time(601), "10m01s");
+        assert_eq!(format_time(610), "10m10s");
+        assert_eq!(format_time(3599), "59m59s");
+        assert_eq!(format_time(3600), "1h00m00s");
+        assert_eq!(format_time(3601), "1h00m01s");
+        assert_eq!(format_time(3610), "1h00m10s");
+        assert_eq!(format_time(3660), "1h01m00s");
+        assert_eq!(format_time(3661), "1h01m01s");
+        assert_eq!(format_time(3670), "1h01m10s");
+        assert_eq!(format_time(4200), "1h10m00s");
+        assert_eq!(format_time(4201), "1h10m01s");
+        assert_eq!(format_time(4210), "1h10m10s");
+        assert_eq!(format_time(36000), "10h00m00s");
+        assert_eq!(format_time(86399), "23h59m59s");
+        assert_eq!(format_time(86400), "1d00h00m00s");
+        assert_eq!(format_time(86401), "1d00h00m01s");
+        assert_eq!(format_time(864_000), "10d00h00m00s");
+        assert_eq!(format_time(8_640_000), "100d00h00m00s");
+        assert_eq!(format_time(86_400_000), "1000d00h00m00s");
+    }
+}

f2c73dab0c4b88a6a309c7524e09acd6cdd072cf
Jesse Luehrs <doy@tozt.net>
2019-10-11 15:16:47 -0400

    add tls support to the server

--- src/cmd/server.rs
+++ src/cmd/server.rs
@@ -1,6 +1,9 @@
 use futures::future::Future as _;
 use futures::stream::Stream as _;
+use snafu::futures01::stream::StreamExt as _;
+use snafu::futures01::FutureExt as _;
 use snafu::ResultExt as _;
+use std::io::Read as _;
 
 #[derive(Debug, snafu::Snafu)]
 pub enum Error {
@@ -19,6 +22,38 @@ pub enum Error {
         input: String,
         source: std::num::ParseIntError,
     },
+
+    #[snafu(display("failed to accept: {}", source))]
+    Acceptor { source: tokio::io::Error },
+
+    #[snafu(display(
+        "failed to send accepted socket to server thread: {}",
+        source
+    ))]
+    SocketChannel {
+        source: tokio::sync::mpsc::error::TrySendError<tokio::net::TcpStream>,
+    },
+
+    #[snafu(display("failed to send accepted socket to server thread"))]
+    TlsSocketChannel {
+        // XXX tokio_tls::Accept doesn't implement Debug or Display
+    // source: tokio::sync::mpsc::error::TrySendError<tokio_tls::Accept<tokio::net::TcpStream>>,
+    },
+
+    #[snafu(display("failed to run server: {}", source))]
+    Server { source: crate::server::Error },
+
+    #[snafu(display("failed to open identity file: {}", source))]
+    OpenIdentityFile { source: std::io::Error },
+
+    #[snafu(display("failed to read identity file: {}", source))]
+    ReadIdentityFile { source: std::io::Error },
+
+    #[snafu(display("failed to parse identity file: {}", source))]
+    ParseIdentity { source: native_tls::Error },
+
+    #[snafu(display("failed to create tls acceptor: {}", source))]
+    CreateAcceptor { source: native_tls::Error },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -38,6 +73,11 @@ pub fn cmd<'a, 'b>(app: clap::App<'a, 'b>) -> clap::App<'a, 'b> {
         .arg(
             clap::Arg::with_name("read-timeout")
                 .long("read-timeout")
+                .takes_value(true),
+        )
+        .arg(
+            clap::Arg::with_name("tls-identity-file")
+                .long("tls-identity-file")
                 .takes_value(true),
         )
 }
@@ -70,34 +110,91 @@ pub fn run<'a>(matches: &clap::ArgMatches<'a>) -> super::Result<()> {
                 .context(super::Server)
         },
     )?;
-    run_impl(address, buffer_size, read_timeout).context(super::Server)
+    let tls_identity_file = matches.value_of("tls-identity-file");
+    run_impl(address, buffer_size, read_timeout, tls_identity_file)
+        .context(super::Server)
 }
 
 fn run_impl(
     address: std::net::SocketAddr,
     buffer_size: usize,
     read_timeout: std::time::Duration,
+    tls_identity_file: Option<&str>,
 ) -> Result<()> {
+    let (acceptor, server) =
+        if let Some(tls_identity_file) = tls_identity_file {
+            create_server_tls(
+                address,
+                buffer_size,
+                read_timeout,
+                tls_identity_file,
+            )?
+        } else {
+            create_server(address, buffer_size, read_timeout)?
+        };
+    tokio::run(futures::future::lazy(move || {
+        tokio::spawn(server.map_err(|e| {
+            eprintln!("{}", e);
+        }));
+
+        acceptor.map_err(|e| {
+            eprintln!("{}", e);
+        })
+    }));
+    Ok(())
+}
+
+fn create_server(
+    address: std::net::SocketAddr,
+    buffer_size: usize,
+    read_timeout: std::time::Duration,
+) -> Result<(
+    Box<dyn futures::future::Future<Item = (), Error = Error> + Send>,
+    Box<dyn futures::future::Future<Item = (), Error = Error> + Send>,
+)> {
     let (mut sock_w, sock_r) = tokio::sync::mpsc::channel(100);
     let listener = tokio::net::TcpListener::bind(&address).context(Bind)?;
     let acceptor = listener
         .incoming()
-        .map_err(|e| {
-            eprintln!("accept failed: {}", e);
-        })
-        .for_each(move |sock| {
-            sock_w.try_send(sock).map_err(|e| {
-                eprintln!("sending socket to manager thread failed: {}", e);
-            })
+        .context(Acceptor)
+        .for_each(move |sock| sock_w.try_send(sock).context(SocketChannel));
+    let server =
+        crate::server::Server::new(buffer_size, read_timeout, sock_r)
+            .context(Server);
+    Ok((Box::new(acceptor), Box::new(server)))
+}
+
+fn create_server_tls(
+    address: std::net::SocketAddr,
+    buffer_size: usize,
+    read_timeout: std::time::Duration,
+    tls_identity_file: &str,
+) -> Result<(
+    Box<dyn futures::future::Future<Item = (), Error = Error> + Send>,
+    Box<dyn futures::future::Future<Item = (), Error = Error> + Send>,
+)> {
+    let (mut sock_w, sock_r) = tokio::sync::mpsc::channel(100);
+    let listener = tokio::net::TcpListener::bind(&address).context(Bind)?;
+
+    let mut file =
+        std::fs::File::open(tls_identity_file).context(OpenIdentityFile)?;
+    let mut identity = vec![];
+    file.read_to_end(&mut identity).context(ReadIdentityFile)?;
+    let identity = native_tls::Identity::from_pkcs12(&identity, "")
+        .context(ParseIdentity)?;
+    let acceptor =
+        native_tls::TlsAcceptor::new(identity).context(CreateAcceptor)?;
+    let acceptor = tokio_tls::TlsAcceptor::from(acceptor);
+
+    let acceptor =
+        listener.incoming().context(Acceptor).for_each(move |sock| {
+            let sock = acceptor.accept(sock);
+            sock_w
+                .try_send(sock)
+                .map_err(|_| Error::TlsSocketChannel {})
         });
-
-    tokio::run(futures::future::lazy(move || {
-        let server =
-            crate::server::Server::new(buffer_size, read_timeout, sock_r)
-                .map_err(|e| eprintln!("{}", e));
-        tokio::spawn(server);
-
-        acceptor
-    }));
-    Ok(())
+    let server =
+        crate::server::TlsServer::new(buffer_size, read_timeout, sock_r)
+            .context(Server);
+    Ok((Box::new(acceptor), Box::new(server)))
 }
--- src/server.rs
+++ src/server.rs
@@ -697,18 +697,15 @@ impl<S: tokio::io::AsyncRead + tokio::io::AsyncWrite + Send + 'static>
     fn poll_new_connections(
         &mut self,
     ) -> Result<crate::component_future::Poll<()>> {
-        match self.sock_stream.poll() {
-            Ok(futures::Async::Ready(Some(conn))) => {
+        match self.sock_stream.poll()? {
+            futures::Async::Ready(Some(conn)) => {
                 self.connections.insert(conn.id.to_string(), conn);
                 Ok(crate::component_future::Poll::DidWork)
             }
-            Ok(futures::Async::Ready(None)) => {
-                Err(Error::SocketChannelClosed)
-            }
-            Ok(futures::Async::NotReady) => {
+            futures::Async::Ready(None) => Err(Error::SocketChannelClosed),
+            futures::Async::NotReady => {
                 Ok(crate::component_future::Poll::NotReady)
             }
-            Err(e) => Err(e),
         }
     }
 
@@ -795,6 +792,125 @@ impl<S: tokio::io::AsyncRead + tokio::io::AsyncWrite + Send + 'static>
 impl<S: tokio::io::AsyncRead + tokio::io::AsyncWrite + Send + 'static>
     futures::future::Future for Server<S>
 {
+    type Item = ();
+    type Error = Error;
+
+    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error> {
+        crate::component_future::poll_future(self, Self::POLL_FNS)
+    }
+}
+
+#[allow(clippy::module_name_repetitions)]
+pub struct TlsServer {
+    server: Server<tokio_tls::TlsStream<tokio::net::TcpStream>>,
+    sock_r:
+        tokio::sync::mpsc::Receiver<tokio_tls::Accept<tokio::net::TcpStream>>,
+    sock_w: tokio::sync::mpsc::Sender<
+        tokio_tls::TlsStream<tokio::net::TcpStream>,
+    >,
+    accepting_sockets: Vec<tokio_tls::Accept<tokio::net::TcpStream>>,
+}
+
+impl TlsServer {
+    pub fn new(
+        buffer_size: usize,
+        read_timeout: std::time::Duration,
+        sock_r: tokio::sync::mpsc::Receiver<
+            tokio_tls::Accept<tokio::net::TcpStream>,
+        >,
+    ) -> Self {
+        let (tls_sock_w, tls_sock_r) = tokio::sync::mpsc::channel(100);
+        Self {
+            server: Server::new(buffer_size, read_timeout, tls_sock_r),
+            sock_r,
+            sock_w: tls_sock_w,
+            accepting_sockets: vec![],
+        }
+    }
+}
+
+impl TlsServer {
+    const POLL_FNS: &'static [&'static dyn for<'a> Fn(
+        &'a mut Self,
+    ) -> Result<
+        crate::component_future::Poll<()>,
+    >] = &[
+        &Self::poll_new_connections,
+        &Self::poll_handshake_connections,
+        &Self::poll_server,
+    ];
+
+    fn poll_new_connections(
+        &mut self,
+    ) -> Result<crate::component_future::Poll<()>> {
+        match self.sock_r.poll().context(SocketChannelReceive)? {
+            futures::Async::Ready(Some(sock)) => {
+                self.accepting_sockets.push(sock);
+                Ok(crate::component_future::Poll::DidWork)
+            }
+            futures::Async::Ready(None) => Err(Error::SocketChannelClosed),
+            futures::Async::NotReady => {
+                Ok(crate::component_future::Poll::NotReady)
+            }
+        }
+    }
+
+    fn poll_handshake_connections(
+        &mut self,
+    ) -> Result<crate::component_future::Poll<()>> {
+        let mut did_work = false;
+        let mut not_ready = false;
+
+        let mut i = 0;
+        while i < self.accepting_sockets.len() {
+            let sock = self.accepting_sockets.get_mut(i).unwrap();
+            match sock.poll() {
+                Ok(futures::Async::Ready(sock)) => {
+                    self.accepting_sockets.swap_remove(i);
+                    self.sock_w.try_send(sock).unwrap_or_else(|e| {
+                        log::warn!(
+                            "failed to send connected tls socket: {}",
+                            e
+                        );
+                    });
+                    did_work = true;
+                    continue;
+                }
+                Ok(futures::Async::NotReady) => {
+                    not_ready = true;
+                }
+                Err(e) => {
+                    log::warn!("failed to accept tls connection: {}", e);
+                    self.accepting_sockets.swap_remove(i);
+                    continue;
+                }
+            }
+            i += 1;
+        }
+
+        if did_work {
+            Ok(crate::component_future::Poll::DidWork)
+        } else if not_ready {
+            Ok(crate::component_future::Poll::NotReady)
+        } else {
+            Ok(crate::component_future::Poll::NothingToDo)
+        }
+    }
+
+    fn poll_server(&mut self) -> Result<crate::component_future::Poll<()>> {
+        match self.server.poll()? {
+            futures::Async::Ready(()) => {
+                Ok(crate::component_future::Poll::DidWork)
+            }
+            futures::Async::NotReady => {
+                Ok(crate::component_future::Poll::NotReady)
+            }
+        }
+    }
+}
+
+#[must_use = "futures do nothing unless polled"]
+impl futures::future::Future for TlsServer {
     type Item = ();
     type Error = Error;
 

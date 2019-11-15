fe4fa53dbbb6030beae2094e33d1db008532ae3c
Jesse Luehrs <doy@tozt.net>
2019-11-15 12:53:58 -0500

    add basic websocket server implementation
    based on example in the gotham repo - ugly, but it's what is supported
    for now

--- src/web.rs
+++ src/web.rs
@@ -1,11 +1,87 @@
-use gotham::router::builder::{DefineSingleRoute as _, DrawRoutes as _};
+mod ws;
 
-pub fn router() -> gotham::router::Router {
+use futures::{Future as _, Sink as _, Stream as _};
+use gotham::router::builder::{DefineSingleRoute as _, DrawRoutes as _};
+use gotham::state::FromState as _;
+
+pub fn router() -> impl gotham::handler::NewHandler {
     gotham::router::builder::build_simple_router(|route| {
         route.get("/").to(root);
+        route.get("/ws").to(handle_websocket_connection);
     })
 }
 
 fn root(state: gotham::state::State) -> (gotham::state::State, String) {
+    log::info!("request for /");
     (state, "hello world".to_string())
+}
+
+fn handle_websocket_connection(
+    mut state: gotham::state::State,
+) -> (gotham::state::State, hyper::Response<hyper::Body>) {
+    let body = hyper::Body::take_from(&mut state);
+    let headers = hyper::HeaderMap::take_from(&mut state);
+    if ws::requested(&headers) {
+        let (response, stream) = match ws::accept(&headers, body) {
+            Ok(res) => res,
+            Err(_) => {
+                log::error!("failed to accept websocket request");
+                return (
+                    state,
+                    hyper::Response::builder()
+                        .status(hyper::StatusCode::BAD_REQUEST)
+                        .body(hyper::Body::empty())
+                        .unwrap(),
+                );
+            }
+        };
+        let req_id = gotham::state::request_id(&state).to_owned();
+        let stream = stream
+            .map_err(|e| {
+                log::error!(
+                    "error upgrading connection for websockets: {}",
+                    e
+                )
+            })
+            .and_then(move |stream| handle_websocket_stream(req_id, stream));
+        tokio::spawn(stream);
+        (state, response)
+    } else {
+        (
+            state,
+            hyper::Response::new(hyper::Body::from(
+                "non-websocket request to websocket endpoint",
+            )),
+        )
+    }
+}
+
+fn handle_websocket_stream<S>(
+    req_id: String,
+    stream: S,
+) -> impl futures::Future<Item = (), Error = ()>
+where
+    S: futures::Stream<
+            Item = tokio_tungstenite::tungstenite::protocol::Message,
+            Error = tokio_tungstenite::tungstenite::Error,
+        > + futures::Sink<
+            SinkItem = tokio_tungstenite::tungstenite::protocol::Message,
+            SinkError = tokio_tungstenite::tungstenite::Error,
+        >,
+{
+    let (sink, stream) = stream.split();
+    sink.send_all(stream.map(move |msg| {
+        handle_websocket_message(&req_id, &msg);
+        msg
+    }))
+    .map_err(|e| log::error!("error during websocket stream: {}", e))
+    .map(|_| log::info!("disconnect"))
+}
+
+fn handle_websocket_message(
+    req_id: &str,
+    msg: &tokio_tungstenite::tungstenite::protocol::Message,
+) {
+    // TODO
+    log::info!("websocket stream message for {}: {:?}", req_id, msg);
 }
---
+++ src/web/ws.rs
@@ -0,0 +1,61 @@
+// from https://github.com/gotham-rs/gotham/blob/master/examples/websocket/src/main.rs
+
+use futures::future::Future as _;
+
+const PROTO_WEBSOCKET: &str = "websocket";
+const SEC_WEBSOCKET_KEY: &str = "Sec-WebSocket-Key";
+const SEC_WEBSOCKET_ACCEPT: &str = "Sec-WebSocket-Accept";
+
+pub fn requested(headers: &hyper::HeaderMap) -> bool {
+    headers.get(hyper::header::UPGRADE)
+        == Some(&hyper::header::HeaderValue::from_static(PROTO_WEBSOCKET))
+}
+
+pub fn accept(
+    headers: &hyper::HeaderMap,
+    body: hyper::Body,
+) -> Result<
+    (
+        hyper::Response<hyper::Body>,
+        impl futures::Future<
+            Item = tokio_tungstenite::WebSocketStream<
+                hyper::upgrade::Upgraded,
+            >,
+            Error = hyper::Error,
+        >,
+    ),
+    (),
+> {
+    let res = response(headers)?;
+    let ws = body.on_upgrade().map(|upgraded| {
+        tokio_tungstenite::WebSocketStream::from_raw_socket(
+            upgraded,
+            tokio_tungstenite::tungstenite::protocol::Role::Server,
+            None,
+        )
+    });
+
+    Ok((res, ws))
+}
+
+fn response(
+    headers: &hyper::HeaderMap,
+) -> Result<hyper::Response<hyper::Body>, ()> {
+    let key = headers.get(SEC_WEBSOCKET_KEY).ok_or(())?;
+
+    Ok(hyper::Response::builder()
+        .header(hyper::header::UPGRADE, PROTO_WEBSOCKET)
+        .header(hyper::header::CONNECTION, "upgrade")
+        .header(SEC_WEBSOCKET_ACCEPT, accept_key(key.as_bytes()))
+        .status(hyper::StatusCode::SWITCHING_PROTOCOLS)
+        .body(hyper::Body::empty())
+        .unwrap())
+}
+
+fn accept_key(key: &[u8]) -> String {
+    const WS_GUID: &[u8] = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    let mut sha1 = sha1::Sha1::default();
+    sha1.update(key);
+    sha1.update(WS_GUID);
+    base64::encode(&sha1.digest().bytes())
+}

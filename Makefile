b5531007a71c3c2664b0d68877ebfee94ec1bb68
Jesse Luehrs <doy@tozt.net>
2019-11-21 15:41:35 -0500

    optimize wasm file

--- Makefile
+++ Makefile
@@ -100,8 +100,11 @@ wasm: teleterm/static/teleterm_web.js teleterm/static/teleterm_web_bg.wasm
 
 web rweb dweb: wasm
 
-teleterm/static/%: target/wasm/%
+teleterm/static/teleterm_web.js: target/wasm/teleterm_web.js
 	@cp -f $< $@
+
+teleterm/static/teleterm_web_bg.wasm: target/wasm/teleterm_web_bg.wasm
+	@wasm-opt $< -o $@
 
 target/wasm/teleterm_web.js target/wasm/teleterm_web_bg.wasm: teleterm-web/Cargo.toml teleterm-web/src/lib.rs
 	@wasm-pack build --no-typescript --target web --out-dir ../target/wasm teleterm-web
--- teleterm/static/teleterm_web.js
+++ teleterm/static/teleterm_web.js
@@ -250,24 +250,6 @@ function init(module) {
             const a = state.a;
             state.a = 0;
             try {
-                return __wbg_elem_binding2(a, state.b, arg0);
-            } finally {
-                if (--state.cnt === 0) wasm.__wbg_function_table.get(3)(a, state.b);
-                else state.a = a;
-            }
-        }
-        ;
-        real.original = state;
-        const ret = real;
-        return addHeapObject(ret);
-    };
-    imports.wbg.__wbindgen_closure_wrapper21 = function(arg0, arg1, arg2) {
-        const state = { a: arg0, b: arg1, cnt: 1 };
-        const real = (arg0) => {
-            state.cnt++;
-            const a = state.a;
-            state.a = 0;
-            try {
                 return __wbg_elem_binding1(a, state.b, arg0);
             } finally {
                 if (--state.cnt === 0) wasm.__wbg_function_table.get(3)(a, state.b);
@@ -280,6 +262,24 @@ function init(module) {
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper19 = function(arg0, arg1, arg2) {
+        const state = { a: arg0, b: arg1, cnt: 1 };
+        const real = (arg0) => {
+            state.cnt++;
+            const a = state.a;
+            state.a = 0;
+            try {
+                return __wbg_elem_binding2(a, state.b, arg0);
+            } finally {
+                if (--state.cnt === 0) wasm.__wbg_function_table.get(3)(a, state.b);
+                else state.a = a;
+            }
+        }
+        ;
+        real.original = state;
+        const ret = real;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_closure_wrapper21 = function(arg0, arg1, arg2) {
         const state = { a: arg0, b: arg1, cnt: 1 };
         const real = (arg0) => {
             state.cnt++;

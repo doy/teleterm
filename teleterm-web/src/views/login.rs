fb08af01454c5dff7690a1cc86718d0d29d01092
Jesse Luehrs <doy@tozt.net>
2019-11-25 12:20:14 -0500

    autofocus the username text box

--- teleterm-web/src/views/login.rs
+++ teleterm-web/src/views/login.rs
@@ -4,9 +4,11 @@ pub(crate) fn render(_: &crate::model::Model) -> Vec<Node<crate::Msg>> {
     vec![seed::form![
         seed::attrs! { At::Action => "#" },
         seed::label![seed::attrs! { At::For => "username" }, "username"],
-        seed::input![
-            seed::attrs! { At::Type => "text", At::Id => "username" }
-        ],
+        seed::input![seed::attrs! {
+            At::Id => "username",
+            At::Type => "text",
+            At::AutoFocus => true.as_at_value(),
+        }],
         seed::input![
             seed::attrs! { At::Type => "submit", At::Value => "login" }
         ],
